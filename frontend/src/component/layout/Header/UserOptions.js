121ccc864d2f9ae0cb27dcfc26b85c1adcf7ab69
Harsh Verma <harshverma6395@gmail.com>
2022-03-30 15:36:06 +0530

    added nodemonassa

--- frontend/src/component/layout/Header/UserOptions.js
+++ frontend/src/component/layout/Header/UserOptions.js
@@ -35,7 +35,7 @@ const UserOptions = ({ user }) => {
     { icon: <ExitToAppIcon />, name: "Logout", func: logoutUser },
   ];
 
-  if (user.role === "admin") {
+  if (user.role !== "admin") {
     options.unshift({
       icon: <DashboardIcon />,
       name: "Dashboard",
