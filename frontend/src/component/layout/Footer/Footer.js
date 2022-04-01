8125f2346518ca7146d0b4768ff7f893c3f9a47f
Harsh Verma <harshverma6395@gmail.com>
2022-04-01 12:56:53 +0530

    kkjnda

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
-import playStore from "../../../images/playstore.png";
-import appStore from "../../../images/Appstore.png";
+// import playStore from "../../../images/playstore.png";
+// import appStore from "../../../images/Appstore.png";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer id="footer">
       <div className="leftFooter">
-        <h4>DOWNLOAD OUR APP</h4>
+        {/* <h4>DOWNLOAD OUR APP</h4>
         <p>Download App for Android and IOS mobile phone</p>
         <img src={playStore} alt="playstore" />
         <img src={appStore} alt="Appstore" />
@@ -24,7 +24,7 @@ const Footer = () => {
         <h4>Follow Us</h4>
         <a href="http://instagram.com/meabhisingh">Instagram</a>
         <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <a href="http://instagram.com/meabhisingh">Facebook</a> */}
       </div>
     </footer>
   );
