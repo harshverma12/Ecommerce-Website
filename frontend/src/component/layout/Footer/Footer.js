e2691601dabff9dfdc28e1ecb2748edc8138d32d
Harsh Verma <harshverma6395@gmail.com>
2022-04-03 20:50:39 +0530

    kks

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -3,13 +3,21 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  // return (
-  //   // <footer id="footer">
-  //   //   <div className="leftFooter">
- 
-  //   //   </div>
-  //   // </footer>
-  // );
+  return (
+    <footer id="footer">
+      <div className="leftFooter">
+  
+       
+      </div>
+
+      <div className="midFooter">
+    
+      </div>
+
+      <div className="rightFooter">
+       </div>
+    </footer>
+  );
 };
 
 export default Footer;
