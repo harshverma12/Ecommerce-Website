aeaad11b87f1d337cb379a38bff481585129e821
Harsh Verma <harshverma6395@gmail.com>
2022-06-27 16:25:52 +0530

    make it betterasasaa

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -143,7 +143,7 @@ const ProductDetails = ({ match }) => {
                   ({product.numOfReviews} Reviews)
                 </span>
 
-
+ 
 
               </div>
 {/* 
@@ -199,6 +199,20 @@ const ProductDetails = ({ match }) => {
               <button onClick={submitReviewToggle} className="submitReview">
                 Submit Review
               </button>
+
+
+              <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
             </div>
           </div>
 
