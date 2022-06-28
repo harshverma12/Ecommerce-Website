5db5005450844b841194dfcb0a6f5c9c68038b9b
Harsh Verma <harshverma6395@gmail.com>
2022-06-28 16:15:42 +0530

    make it betterasasaaaaa

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -13,9 +13,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
     <>
        <nav class=" navbar navbar-dark bg-primary">
   <a class="navbar-brand" href="/">CollegeNest</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+
+  {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+    
+  </button> */}
+
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="/nav-item nav-link/" href="/">Home <span class="sr-only"></span></a>
