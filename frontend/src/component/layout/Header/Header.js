f6f04497a1297639f61faf307cde31ac0bbf4be2
Harsh Verma <harshverma6395@gmail.com>
2022-06-28 16:47:13 +0530

    make it betterasasaaaaa

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -13,16 +13,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
     <>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/">CollegeNest</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
+
+
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link" href="/products/">Products</a>
+        {/* <a class="nav-link" href="#">Cart</a> */}
+        <a class="nav-link" href="/login/">Login</a>
+
+        {/* <a class="nav-link disabled">Disabled</a> */}
       </div>
     </div>
   </div>
