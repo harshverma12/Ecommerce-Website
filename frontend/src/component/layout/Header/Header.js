5c95d4012f26ef95b1f96ffa386df0170fd908b5
Harsh Verma <harshverma6395@gmail.com>
2022-06-28 15:34:47 +0530

    make it betterasasaa

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -10,21 +10,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
   const Header = () => {
   return (
-
-
     <>
-       
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+       <nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">CollegeNest</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+      <a class="/" href="#">Home <span class="sr-only"></span></a>
+      <a class="/products/" href="#">Products</a>
+      <a class="/cart/" href="#">Cart</a>
+      <a class="/login/" href="#">Login</a>
     </div>
   </div>
 </nav>
