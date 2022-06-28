d7b4b205094135bc0cef44ae2d1463a90f9c35ba
Harsh Verma <harshverma6395@gmail.com>
2022-06-28 16:26:04 +0530

    make it betterasasaaaaa

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -11,14 +11,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
   const Header = () => {
   return (
     <>
+
+<nav class="navbar navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+{/*  
        <nav class=" navbar navbar-dark bg-primary">
   <a class="navbar-brand" href="/">CollegeNest</a>
 
-  {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     
-  </button> */}
-
+  </button> 
+ 
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="/nav-item nav-link/" href="/">Home <span class="sr-only"></span></a>
@@ -27,7 +46,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
       <a class="/nav-item nav-link/" href="/login/">Login</a>
     </div>
   </div>
-</nav>
+</nav>   */}
+
+
+
       </>
   )
 }
